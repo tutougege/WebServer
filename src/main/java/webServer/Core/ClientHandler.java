9d3a0fe297877aa8be7e8951bea020ce3930e12f
李葳 <401613869@qq.com>
2022-04-07 11:55:58 +0800

    Part03

--- src/main/java/webServer/Core/ClientHandler.java
+++ src/main/java/webServer/Core/ClientHandler.java
@@ -2,10 +2,9 @@ package webServer.Core;
 
 import webServer.Http.HttpServletRequest;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -22,8 +21,24 @@ public class ClientHandler implements Runnable{
             //处理请求
 
             //发送响应
+            File file = new File("./webApps/MyWeb.index.html");
+            //发送状态行
+            String line = "HTTP/1.1 200 OK";
+            printLine(line);
+            //发送响应头
+            line = "Content-Type: Text/html";
+            printLine(line);
+            line = "Content-Length"+file.length();
+            printLine(line);
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+    public void printLine(String line) throws IOException {
+            OutputStream os = socket.getOutputStream();
+            byte[] bytes = line.getBytes(StandardCharsets.ISO_8859_1);
+            os.write(bytes);
+            os.write((char)13);//发送回车符
+            os.write((char)10);//发送换行符
     }
 }
